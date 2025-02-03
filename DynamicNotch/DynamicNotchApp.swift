b818bbda9c63b40c071d81a26139e153a478c091
PeterPark1221 <137192346+PeterPark1221@users.noreply.github.com>
2025-02-03 15:42:50 +0900

    Initial Commit

---
+++ DynamicNotch/DynamicNotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicNotchApp.swift
+//  DynamicNotch
+//
+//  Created by PeterPark on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct DynamicNotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
