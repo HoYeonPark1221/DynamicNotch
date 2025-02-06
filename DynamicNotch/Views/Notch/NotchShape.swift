ed6854fc4e17db8981e3dc028082c386aa78a963
PeterPark1221 <137192346+PeterPark1221@users.noreply.github.com>
2025-02-07 00:41:08 +0900

    create notch

--- DynamicNotch/Views/Notch/NotchShape.swift
+++ DynamicNotch/Views/Notch/NotchShape.swift
@@ -8,50 +8,31 @@ import SwiftUI
 
 struct NotchShape: Shape {
     
-    var topCornerRadius: CGFloat = 20
-    
-    var bottomCornerRadius: CGFloat = 15
+    var topCorner: CGFloat = 5
+    var bottomCorner: CGFloat = 10
     
     func path(in rect: CGRect) -> Path {
-        
-        Path { path in  //
-            path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        Path { path in
+            path.move(to: CGPoint(x: rect.minX, y: rect.minY)) //(0,0)
             
-            // Top left inner curve
-            path.addQuadCurve(
-                to: CGPoint(x: rect.minX + topCornerRadius, y: rect.minY + topCornerRadius),
-                control: CGPoint(x: rect.minX + topCornerRadius, y: rect.minY)
-            )
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY)) //(200, 0)
             
-            // Left vertical line
-            path.addLine(to: CGPoint(x: rect.minX + topCornerRadius, y: rect.maxY - bottomCornerRadius))
+            path.addQuadCurve(to: CGPoint(x: rect.maxX - topCorner, y: rect.minY + topCorner), control: CGPoint(x: rect.maxX - topCorner, y: rect.minY)) //(195, 5) & 제어점 (195,0)
+            //
+            path.addLine(to: CGPoint(x: rect.maxX - topCorner, y: rect.maxY - bottomCorner)) //(195, 22)
             
-            // Bottom left corner
-            path.addQuadCurve(
-                to: CGPoint(x: rect.minX + topCornerRadius + bottomCornerRadius, y: rect.maxY),
-                control: CGPoint(x: rect.minX + topCornerRadius, y: rect.maxY)
-            )
+            path.addQuadCurve(to: CGPoint(x: rect.maxX - topCorner - bottomCorner , y: rect.maxY), control: CGPoint(x: rect.maxX - topCorner, y: rect.maxY)) // (185, 32)
             
-            // Bottom horizontal line
-            path.addLine(to: CGPoint(x: rect.maxX - topCornerRadius - bottomCornerRadius, y: rect.maxY))
             
-            // Bottom right corner
-            path.addQuadCurve(
-                to: CGPoint(x: rect.maxX - topCornerRadius, y: rect.maxY - bottomCornerRadius),
-                control: CGPoint(x: rect.maxX - topCornerRadius, y: rect.maxY)
-            )
+            //하단 평행선
+            path.addLine(to: CGPoint(x: rect.minX + topCorner + bottomCorner, y: rect.maxY)) //(15, 32)
             
-            // Right vertical line
-            path.addLine(to: CGPoint(x: rect.maxX - topCornerRadius, y: rect.minY + topCornerRadius))
+            path.addQuadCurve(to: CGPoint(x: rect.minX + topCorner, y: rect.maxY - bottomCorner), control: CGPoint(x: rect.minX + topCorner, y: rect.maxY)) //(5, 22)
             
-            // Top right inner curve
-            path.addQuadCurve(
-                to: CGPoint(x: rect.maxX, y: rect.minY),
-                control: CGPoint(x: rect.maxX - topCornerRadius, y: rect.minY)
-            )
+            path.addLine(to: CGPoint(x: rect.minX + topCorner, y: rect.minY + topCorner))
             
-            // Closing the path
-            path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+            path.addQuadCurve(to: CGPoint(x: rect.minX, y: rect.minY), control: CGPoint(x: rect.minX + topCorner, y: rect.minY))
+            
         }
     }
 }
